cffb7b2e0c1d7a907720122f189ca99734bc075b
Giorgia Pitteri <g.pitteri@appliedai.de>
2024-05-22 11:27:29 +0200

    wip: featureflow uses fit method from base class. Added to self.trainable_layers the ones from the pretrained feature encoder.

--- src/nf4ad/flows.py
+++ src/nf4ad/flows.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Union, Tuple
+from typing import Any, Dict, Union, Tuple, Optional
 import logging
 
 from src.veriflow.flows import Flow
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset
 from torch.nn import Module
 import numpy as np
 import torchvision.transforms as transforms
-from src.nf4ad.feature_encoder import FeatureEncoder
+from src.nf4ad.feature_encoder import FeatureEncoder, feature_encoder_transform
 
 class FeatureFlow(Flow):
     def __init__(
@@ -38,23 +38,23 @@ class FeatureFlow(Flow):
             # TODO exception
             logging.ERROR("Need a path to the tar folder")
      
+        # TODO: check how to change the way trainable layers is created. Here needs to be flow layers + feature encoder layer
+        self.trainable_layers = torch.nn.ModuleList(
+            [l for l in flow.layers if isinstance(l, torch.nn.Module)] + 
+            [fe_l for _, fe_l in self.feature_encoder.named_modules() if isinstance(fe_l, torch.nn.Module)]
+        )
         
-    def log_prob(self, x):
+    def log_prob(self, x: torch.Tensor, context: Optional[torch.Tensor] = None) -> torch.Tensor:
         """Computes the log probability of self.feature_encoder(x).
         
         Args:
             x (Tensor): The batch of samples.
-        
+            digit (int): Normal class.
         Returns:
             Tensor: The log probability of the samples.
-        """
-        
-        if len(x.shape) == 2:
-            # TODO: values are hardcoded. need to avoid this.
-            x = x.reshape(x.shape[0], 1, 28, 28)
-            
-        #z = self.feature_encoder(featutre_encoder_transform(x))
-        z = self.feature_encoder(x)
+        """ 
+        # TODO: understand how to pass the digit number to find the right min max values for normalization. need to change something in the base class fit module
+        z = self.feature_encoder(feature_encoder_transform(x.unsqueeze(1), digit=3))
         return self.flow.log_prob(z.reshape(z.shape[0], -1))
     
     def to(self, device: torch.device):
@@ -70,107 +70,109 @@ class FeatureFlow(Flow):
         self.feature_encoder.to(device)
         return self 
 
-    def fit(
-            self,
-            data_train: Dataset,
-            optim: torch.optim.Optimizer = torch.optim.Adam,
-            optim_params: Dict[str, Any] = None,
-            batch_size: int = 32,
-            shuffle: bool = True,
-            gradient_clip: float = None,
-            device: torch.device = None,
-            epochs: int = 1,
-        ) -> float:
-            """
-            Wrapper function for the fitting procedure. Allows basic configuration of the optimizer and other
-            fitting parameters.
+    
+    # def fit(
+    #         self,
+    #         data_train: Dataset,
+    #         optim: torch.optim.Optimizer = torch.optim.Adam,
+    #         optim_params: Dict[str, Any] = None,
+    #         batch_size: int = 32,
+    #         shuffle: bool = True,
+    #         gradient_clip: float = None,
+    #         device: torch.device = None,
+    #         epochs: int = 1,
+    #     ) -> float:
+    #         """
+    #         Wrapper function for the fitting procedure. Allows basic configuration of the optimizer and other
+    #         fitting parameters.
 
-            Args:
-                data_train: training data.
-                batch_size: number of samples per optimization step.
-                optim: optimizer class.
-                optimizer_params: optimizer parameter dictionary.
-                jitter: Determines the amount of jitter that is added if the optimization leaves the feasible region.
-                epochs: number of epochs.
+    #         Args:
+    #             data_train: training data.
+    #             batch_size: number of samples per optimization step.
+    #             optim: optimizer class.
+    #             optimizer_params: optimizer parameter dictionary.
+    #             jitter: Determines the amount of jitter that is added if the optimization leaves the feasible region.
+    #             epochs: number of epochs.
 
-            Returns:
-                Loss curve .
-            """
-            if device is None:
-                if torch.backends.mps.is_available():
-                    device = torch.device("mps")
-                elif torch.cuda.is_available():
-                    device = torch.device("cuda:0")
-                else:
-                    device = torch.device("cpu")
+    #         Returns:
+    #             Loss curve .
+    #         """
+    #         if device is None:
+    #             if torch.backends.mps.is_available():
+    #                 device = torch.device("mps")
+    #             elif torch.cuda.is_available():
+    #                 device = torch.device("cuda:0")
+    #             else:
+    #                 device = torch.device("cpu")
 
-            model = self.to(device)
-            print(model)
+    #         model = self.to(device)
+    #         print(model)
 
-            if optim_params is not None:
-                optim = optim(model.parameters(), **optim_params) 
-            else:
-                optim = optim(model.parameters())
+    #         if optim_params is not None:
+    #             optim = optim(model.parameters(), **optim_params) 
+    #         else:
+    #             optim = optim(model.parameters())
 
-            # for param in model.parameters():
-            #     if param.requires_grad:
-            #         print(f"MODEL PARAM {param}")
-            N = len(data_train)
+    #         # for param in model.parameters():
+    #         #     if param.requires_grad:
+    #         #         print(f"MODEL PARAM {param}")
+    #         N = len(data_train)
 
-            epoch_losses = []
-            for _ in range(epochs):
-                losses = []
-                if shuffle:
-                    perm = np.random.choice(N, N, replace=False)
-                    data_train_shuffle = data_train[perm][0]
+    #         epoch_losses = []
+    #         for _ in range(epochs):
+    #             losses = []
+    #             if shuffle:
+    #                 perm = np.random.choice(N, N, replace=False)
+    #                 data_train_shuffle = data_train[perm][0]
 
-                for idx in range(0, N, batch_size):
-                    end = min(idx + batch_size, N)
-                    try:
-                        sample_flatten = torch.Tensor(data_train_shuffle[idx:end]).to(device) # batch of 32x784 (img is an array 28x28 = 784)
-                    except:
-                        continue
+    #             print(f"DATA TRAIN SHAPE {data_train_shuffle.shape}")
+    #             for idx in range(0, N, batch_size):
+    #                 end = min(idx + batch_size, N)
+    #                 try:
+    #                     sample_flatten = torch.Tensor(data_train_shuffle[idx:end]).to(device) # batch of 32x784 (img is an array 28x28 = 784)
+    #                 except:
+    #                     continue
  
-                    # TODO: 28 is hardcoded. maybe put in the config mnist shape (or check if it is in the dataset field)
-                    sample_reshaped = sample_flatten.reshape(sample_flatten.shape[0], 28, 28).unsqueeze(1)
-                    # FIXME: remove hardcoded values
-                    #sample_feat = self.feature_encoder(feature_encoder_transform(sample_reshaped, min_max=(-0.7645772083211267, 12.895051191467457)))
-                    sample_feat = self.feature_encoder(sample_reshaped)
+    #                 # TODO: 28 is hardcoded. maybe put in the config mnist shape (or check if it is in the dataset field)
+    #                 sample_reshaped = sample_flatten.reshape(sample_flatten.shape[0], 28, 28).unsqueeze(1)
+    #                 # FIXME: remove hardcoded values
+    #                 #sample_feat = self.feature_encoder(feature_encoder_transform(sample_reshaped, min_max=(-0.7645772083211267, 12.895051191467457)))
+    #                 sample_feat = self.feature_encoder(sample_reshaped)
                     
-                    sample = sample_feat.reshape(sample_feat.shape[0], -1)
+    #                 sample = sample_feat.reshape(sample_feat.shape[0], -1)
                     
-                    if self.soft_training:
-                        noise = self.training_noise_prior.sample([sample.shape[0]]) 
+    #                 if self.soft_training:
+    #                     noise = self.training_noise_prior.sample([sample.shape[0]]) 
 
-                        # Repeat noise for all data dimensions
-                        sigma = noise
-                        r = torch.Tensor(list(sample.shape[1:])).prod().int()
-                        sigma = sigma.repeat_interleave(r)
-                        sigma = sigma.reshape(sample.shape)
+    #                     # Repeat noise for all data dimensions
+    #                     sigma = noise
+    #                     r = torch.Tensor(list(sample.shape[1:])).prod().int()
+    #                     sigma = sigma.repeat_interleave(r)
+    #                     sigma = sigma.reshape(sample.shape)
 
-                        e = torch.normal(torch.zeros_like(sigma), sigma).to(device)
-                        sample = sample + e
-                        noise = noise.unsqueeze(-1)
-                        noise = noise.detach().to(device)
-                        # scaling of noise for the conditioning recommended by SoftFlow paper
-                        noise = noise * 2/self.training_noise_prior.high
-                    else:
-                        noise = None
+    #                     e = torch.normal(torch.zeros_like(sigma), sigma).to(device)
+    #                     sample = sample + e
+    #                     noise = noise.unsqueeze(-1)
+    #                     noise = noise.detach().to(device)
+    #                     # scaling of noise for the conditioning recommended by SoftFlow paper
+    #                     noise = noise * 2/self.training_noise_prior.high
+    #                 else:
+    #                     noise = None
 
-                    optim.zero_grad()
+    #                 optim.zero_grad()
 
-                    loss = -model.log_prob(
-                        sample_reshaped #, context=noise
-                    ).mean() - model.flow.log_prior()
-                    loss.backward()
-                    losses.append(float(loss.detach()))
-                    if gradient_clip is not None:
-                        torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clip)
-                    optim.step()
-                    if not self.is_feasible():
-                        raise RuntimeError("Model is not invertible")
+    #                 loss = -model.log_prob(
+    #                     sample_reshaped #, context=noise
+    #                 ).mean() - model.flow.log_prior()
+    #                 loss.backward()
+    #                 losses.append(float(loss.detach()))
+    #                 if gradient_clip is not None:
+    #                     torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clip)
+    #                 optim.step()
+    #                 if not self.is_feasible():
+    #                     raise RuntimeError("Model is not invertible")
 
-                    model.flow.transform.clear_cache()
-                epoch_losses.append(np.mean(losses))
+    #                 model.flow.transform.clear_cache()
+    #             epoch_losses.append(np.mean(losses))
 
-            return epoch_losses
+    #         return epoch_losses
