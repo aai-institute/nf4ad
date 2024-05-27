8487f1c5ffd9a8f603611b28cd110de32b61d2bf
Giorgia Pitteri <g.pitteri@appliedai.de>
2024-05-27 13:22:17 +0200

    wip: evaluation module

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ import numpy as np
 from src.explib.visualization import latent_radial_qqplot
 from src.nf4ad.flows import FeatureFlow
 from visualization import latent_radial_qqplot as latent_radial_qqplot_feat_encoder
-def show_imgs(imgs, title=None, row_size=4):
+def show_imgs(imgs, saveto, title=None, row_size=10): # TODO: decide default row_size and pass it from the main code based on number of samples
      
     # Form a grid of pictures (we use max. 8 columns)
     num_imgs = imgs.shape[0] if isinstance(imgs, torch.Tensor) else len(imgs)
@@ -18,21 +18,26 @@ def show_imgs(imgs, title=None, row_size=4):
     is_int = imgs.dtype==torch.int32 if isinstance(imgs, torch.Tensor) else imgs[0].dtype==torch.int32
     nrow = min(num_imgs, row_size)
     ncol = int(math.ceil(num_imgs/nrow))
-
     imgs = torchvision.utils.make_grid(imgs, nrow=nrow, pad_value=128 if is_int else 0.5)
+    print(imgs.shape)
+   
     np_imgs = imgs.cpu().numpy()
- 
+    print(np_imgs.shape)
     # Plot the grid
     plt.figure(figsize=(1.5*nrow, 1.5*ncol))
-    plt.imshow(np.transpose(np_imgs, (1,2,0)), interpolation='nearest')
+    plt.imshow(np.transpose(np_imgs, (1,2, 0)), interpolation='nearest')
     plt.axis('off')
     if title is not None:
         plt.title(title)
+    
+    if saveto:
+        plt.savefig(saveto, title)
+    
     plt.show()
     plt.close()
     
 class Evaluation():
-    """Hyperparameter optimization experiment."""
+    """Evaluation."""
 
     def __init__(
         self,
@@ -50,7 +55,7 @@ class Evaluation():
         self.device = device
     
 
-    def conduct(self, report_dir: os.PathLike, device: torch.device = torch.device("cpu")):
+    def conduct(self, report_dir: os.PathLike, device: torch.device = torch.device("cpu"), n_samples = 100, saveto=None): # TODO: put n_sampeles as an input arg
         """Run the evaluation experiment.
 
         Args:
@@ -65,54 +70,52 @@ class Evaluation():
          
         # Load model
         state_dict = torch.load(os.path.join(report_dir, "best_model.pt"), map_location=torch.device(self.device))
+        
         model_hparams = self.config["model_cfg"]["params"]
+        print(f"MODEL PARAMS {model_hparams}")
+        print(self.config["model_cfg"]["type"])
         model = self.config["model_cfg"]["type"](**model_hparams)
         model.load_state_dict(state_dict)
          
         # Evaluate best model
-        self._test_best_model(model, data_test, device)
+        self._test_best_model(model, data_test, device, n_samples, saveto=saveto)
         
         # QQplots
-        self._qqplot(model, data_test)
+        self._qqplot(model, data_test, n_samples, saveto=saveto)
   
     
     # TODO: add type of input arguments
-    def _test_best_model(self, best_model, data, device, im_shape=(28, 28)):
+    def _test_best_model(self, best_model, data, device, n_samples, im_shape=(28, 28), saveto=None):
          
-        sample = best_model.sample(sample_shape=[1]).cpu().detach().numpy()
-        
+        samples = best_model.sample(sample_shape=[n_samples]).cpu().detach().numpy() 
         if isinstance(best_model, FeatureFlow):
-            sample = sample.squeeze()
+            samples = samples.squeeze()
         else:
-            sample = sample.reshape(*im_shape) #sample_shape=[1]
+            samples = samples.reshape(-1, *im_shape) #sample_shape=[1]
         
-            
+        samples = np.uint8(np.clip(samples, 0, 1) * 255)
         
-         
-            
-        # np.save("sample.npy", sample)
-        sample = np.uint8(np.clip(sample, 0, 1) * 255)
-            
-        plt.imshow(sample, cmap="gray")
+        show_imgs(torch.tensor(samples).unsqueeze(1), saveto)
+       
+        plt.imshow(samples[0], cmap="gray")
         plt.show()
-    
+        
         # Compute the test loss
-        # test_loss = 0
-        # for i in range(0, len(data), 100):
-        #     j = min([len(data), i + 100])
-        #     test_loss += float(
-        #         -best_model.log_prob(data[i:j][0].to(device)).sum()
-        #     )
-        # test_loss /= len(data)
+        test_loss = 0
+        for i in range(0, len(data), 100):
+            j = min([len(data), i + 100])
+            test_loss += float(
+                -best_model.log_prob(data[i:j][0].to(device)).sum()
+            )
+        test_loss /= len(data)
         
-        # print(test_loss)
+        print(test_loss)
         
-    def _qqplot(self, best_model, data, p=1, n_samples=4, save_to = None):
+    def _qqplot(self, best_model, data, n_samples, p=1, saveto = None):
         
-        print(type(best_model))
         if isinstance(best_model, FeatureFlow):
-            latent_radial_qqplot_feat_encoder({"best_model": best_model}, data, p, n_samples, save_to)
+            latent_radial_qqplot_feat_encoder({"best_model": best_model}, data, p, n_samples, saveto)
         else:
-            latent_radial_qqplot({"best_model": best_model}, data, p, n_samples, save_to)
+            latent_radial_qqplot({"best_model": best_model}, data, p, n_samples, saveto)
         
       
