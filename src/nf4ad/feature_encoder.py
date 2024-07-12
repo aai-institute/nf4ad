f978039fe7c565d7a6d859e431c0861e42f3719b
giorgiapitteri <giorgia.pitteri@gmail.com>
2024-07-12 14:43:06 +0200

    wip: LatentFlow

--- src/nf4ad/feature_encoder.py
+++ src/nf4ad/feature_encoder.py
@@ -62,6 +62,8 @@ def feature_encoder_transform(x: torch.Tensor, digit: int = 3):
     
     return transform(x)
 
+# TODO: probably we won't need this FeatureEncoder anymore or 
+# can be refactored to accept the pretrained encoder, pretrained decoder and the mean net (fc1)
 class FeatureEncoder(torch.nn.Module):
     
     def __init__(self):
@@ -106,5 +108,64 @@ class FeatureEncoder(torch.nn.Module):
         x = F.interpolate(F.leaky_relu(self.bn4(x)), scale_factor=2)
         x = self.deconv3(x)
         x = torch.sigmoid(x)
+        
+        return x
+    
+    
+class PretrainedEncoder(torch.nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.rep_dim = 32
+        self.pool = torch.nn.MaxPool2d(2, 2)
+ 
+        self.conv1 = torch.nn.Conv2d(1, 8, 5, bias=False, padding=2)
+        self.bn1 = torch.nn.BatchNorm2d(8, eps=1e-04, affine=False)
+        self.conv2 = torch.nn.Conv2d(8, 4, 5, bias=False, padding=2)
+        self.bn2 = torch.nn.BatchNorm2d(4, eps=1e-04, affine=False)
+        
+    def forward(self, x):
+        
+        x = self.conv1(x)
+        x = self.pool(F.leaky_relu(self.bn1(x)))
+        x = self.conv2(x)
+        x = self.pool(F.leaky_relu(self.bn2(x)))
+        x = x.view(x.size(0), -1)
+         
+        return x
+        
+class PretrainedDecoder(torch.nn.Module):
+       def __init__(self):
+        super().__init__() 
+        self.deconv1 = torch.nn.ConvTranspose2d(2, 4, 5, bias=False, padding=2)
+        self.bn3 = torch.nn.BatchNorm2d(4, eps=1e-04, affine=False)
+        self.deconv2 = torch.nn.ConvTranspose2d(4, 8, 5, bias=False, padding=3)
+        self.bn4 = torch.nn.BatchNorm2d(8, eps=1e-04, affine=False)
+        self.deconv3 = torch.nn.ConvTranspose2d(8, 1, 5, bias=False, padding=2)
+        
+        def forward(self, x):
+         
+            x = x.view(x.size(0), int(self.rep_dim / 16), 4, 4)
+            x = F.interpolate(F.leaky_relu(x), scale_factor=2)
+            x = self.deconv1(x)
+            x = F.interpolate(F.leaky_relu(self.bn3(x)), scale_factor=2)
+            x = self.deconv2(x)
+            x = F.interpolate(F.leaky_relu(self.bn4(x)), scale_factor=2)
+            x = self.deconv3(x)
+            x = torch.sigmoid(x)
+        
+            return x
+    
+class MeanNet(torch.nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.rep_dim = 32
+        self.fc1 = torch.nn.Linear(4 * 7 * 7, self.rep_dim, bias=False)
+        
+    def forward(self, x):
+        x = self.fc1(x)
         
         return x
--- src/nf4ad/flows.py
+++ src/nf4ad/flows.py
@@ -9,7 +9,12 @@ from torch.utils.data import Dataset
 from torch.nn import Module
 import numpy as np
 import torchvision.transforms as transforms
-from nf4ad.feature_encoder import FeatureEncoder, feature_encoder_transform
+from nf4ad.feature_encoder import PretrainedEncoder, PretrainedDecoder, MeanNet, feature_encoder_transform
+import pyro 
+from pyro.infer import SVI
+from pyro.optim import Adam 
+from src.veriflow.flows import Flow
+from pyro import distributions as dist
 
 class FeatureFlow(Flow):
     def __init__(
@@ -93,24 +98,26 @@ class FeatureFlow(Flow):
       
         return y
 
-
 class Encoder(torch.nn.Module):
     def __init__(
         self, 
         dim: Optional[int] = None,
         z_dim: Optional[int] = None, 
-        hidden_dims: Optional[List[int]] = None,
-        hidden_net: Optional[torch.nn.Module] = None,
-        mean_net: Optional[torch.nn.Module] = None,
-        std_net: Optional[torch.nn.Module] = None
+        hidden_dims: Optional[list[int]] = None,
+        hidden_net: Optional[Union[torch.nn.Module, PretrainedEncoder, PretrainedDecoder] ] = None,
+        mean_net: Optional[Union[torch.nn.Module, MeanNet]] = None,
+        std_net: Optional[torch.nn.Module] = None, 
         nonlinearity = torch.nn.Softplus()
         ):
         
+    
+        super().__init__()
         if hidden_net:
             # Using an hard coded architecture to use Tim's pretrained weights 
             self.hidden_net = hidden_net
             self.mean_net = mean_net #fc1 from Tim
-            self.std_net = std_net
+            # TODO: this is done just for having the code run. The std net should be initialize with a fc layer of all zeros
+            self.std_net = mean_net 
             # Tim's network has only one last layer for the mean. We would need to add one (init with all zeros) for std.
         else:
             layers = []
@@ -119,37 +126,196 @@ class Encoder(torch.nn.Module):
             for i in range(len(hidden_dims) - 1):
                 layers.append(torch.nn.Linear(hidden_dims[i], hidden_dims[i+1]))
                 layers.append(nonlinearity)
+
             self.hidden_net = torch.nn.Sequential(*layers)
 
             self.mean_net = torch.nn.Linear(hidden_dims[-1], z_dim)
             self.std_net = torch.nn.Linear(hidden_dims[-1], z_dim)
+ 
 
-
-        super().__init__()
-        
             
     def forward(self, x):
         hidden = self.hidden_net(x)
         mean = self.mean_net(hidden)
-        relu = torch.nn.relu
-        std = relu(self.std_net(hidden))
-
+        relu = torch.nn.ReLU()
+        std = relu(self.std_net(hidden)) + 1e-6
+        
         return mean, std
 
-class Decoder(torch.nn.module):
-    pass
-
-class LatentFlow(Flow):
-    
-    def __init__(self, encoder: Union[Encoder, str], decoder: Union[Decoder, str]):
+class Decoder(torch.nn.Module):
+     
+    def __init__(
+        self, 
+        dim: Optional[int] = None,
+        z_dim: Optional[int] = None, 
+        hidden_dims: Optional[list[int]] = None,
+        net: Optional[torch.nn.Module] = None,
+        nonlinearity = torch.nn.Softplus(),
+        ):
         
+        super().__init__()
+        # TODO: do we want to use the pretrained weights also for the decoder? Only the mean we need at the beginning right?
+        if net:
+            # Using an hard coded architecture to use Tim's pretrained weights 
+            self.net = net
+        else:
+            layers = []
+            layers.append(torch.nn.Linear(z_dim, hidden_dims[-1]))
+            layers.append(nonlinearity)
+            for i in reversed(range(1, len(hidden_dims))):
+                layers.append(torch.nn.Linear(hidden_dims[i], hidden_dims[i-1]))
+                layers.append(nonlinearity)
+                
+            layers.append(torch.nn.Linear(hidden_dims[0], dim))
+            self.net = torch.nn.Sequential(*layers)
+         
+            
+    def forward(self, x):
+        x = self.net(x) 
+ 
+        activation = torch.nn.Sigmoid()
+        x = activation(x)
+
+        return x
+    
+class LatentFlow():
+    
+    def __init__(self, flow: Flow, encoder: Union[Encoder, str], decoder: Union[Decoder, str], mean_net: Optional[Union[MeanNet, str]] = None):
+         
         if isinstance(encoder, Encoder):
             self.encoder = encoder
         else:
-            self. = FeatureEncoder() 
-        
-            checkpoint = torch.load(os.path.abspath(pretrained_feature_encoder))
-            self.feature_encoder.load_state_dict(checkpoint["ae_net_dict"])
+            self.encoder = Encoder(hidden_net=torch.load(encoder), mean_net=torch.load(mean_net))
             
+        if isinstance(decoder, Decoder):
+            self.decoder = decoder
+        else:
+           self.decoder = Decoder(net=torch.load(decoder))
+           
+        self.flow = flow
             
+    # define the model p(x|z)p(z)
+    def model(self, x):
+        # register PyTorch module `decoder` with Pyro
+        pyro.module("decoder", self.decoder)
+        with pyro.plate("data", x.shape[0]):
+            # setup hyperparameters for prior p(z)
+            # z_loc = x.new_zeros(torch.Size((x.shape[0], self.z_dim)))
+            # z_scale = x.new_ones(torch.Size((x.shape[0], self.z_dim)))
+            # z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+            
+            # sample from prior (value will be sampled by guide when computing the ELBO)
+            
+            # TODO: Is this conceptual right? understand how to use pyro and sample from the flow
+            z = self.flow.sample([x.shape[0]])
+            # decode the latent code z
+            loc_img = self.decoder(z)
+
+            # TODO: hadt to put the workaround validate_args=False otherwise I get an error on data range [0, 1]
+            pyro.sample("obs", dist.Bernoulli(loc_img, validate_args=False).to_event(1), obs=x.reshape(-1, 784))
+            
+    # define the guide (i.e. variational distribution) q(z|x)
+    def guide(self, x):
+        # register PyTorch module `encoder` with Pyro
+        pyro.module("encoder", self.encoder)
+        with pyro.plate("data", x.shape[0]):
+            # use the encoder to get the parameters used to define q(z|x)
+            z_loc, z_scale = self.encoder(x)
+            # sample the latent code z
+            lambda_dist = lambda loc, scale: dist.Normal(loc, scale) 
+            pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+            
+     # define a helper function for reconstructing images
+    def reconstruct_img(self, x):
+        # encode image x
+        z_loc, z_scale = self.encoder(x)
+        # sample in latent space
+        z = dist.Normal(z_loc, z_scale).sample()
+        # decode the image (note we don't sample in image space)
+        loc_img = self.decoder(z)
+        return loc_img
+    
+    def to(self, device) -> None:
         
+        self.device = device
+        
+        self.encoder.to(device)
+        self.decoder.to(device)
+        self.flow.to(device)
+        
+        return device
+        
+    def fit(self, data_train: Dataset,
+            svi: SVI, 
+            batch_size: int = 32,
+            shuffle: bool = True,
+            gradient_clip: float = True,
+            device: torch.device = None,
+            epochs: int = 1):
+        
+        if device is None:
+            if torch.backends.mps.is_available():
+                device = torch.device("mps")
+            elif torch.cuda.is_available():
+                device = torch.device("cuda:0")
+            else:
+                device = torch.device("cpu")
+ 
+        model = self.to(device)
+        
+        N = len(data_train)
+        epoch_losses = []
+        for _ in range(epochs):
+            losses = []
+            if shuffle:
+                perm = np.random.choice(N, N, replace=False)
+                data_train_shuffle = data_train[perm][0]
+
+            for idx in range(0, N, batch_size):
+                end = min(idx + batch_size, N)
+                try:
+                    sample = torch.Tensor(data_train_shuffle[idx:end]).to(device)
+                except:
+                    continue
+
+                # TODO: Using svi step this is not needed anymore. Check if it is taken care inside the svi.step
+                # optimizer.zero_grad()
+
+                loss = svi.step(sample)
+                
+                losses.append(loss)
+                
+                # # TODO: check if this is needed here 
+                # if gradient_clip is not None:
+                #     torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clip)
+                
+                # TODO: this is also related to the explanation in line 281
+                #optim.step()
+                
+                # TODO: for the moment LatentFlow doesn't inherit from Flow --> therefore this method doesn't exist. 
+                # Check if we need to implement it also for the LatentFlow
+                # if not self.is_feasible():
+                #     raise RuntimeError("Model is not invertible")
+
+                
+            epoch_losses.append(np.mean(losses))
+
+        return epoch_losses
+
+        # TODO: this is done outside the fit method for the Latentflow --> check this!
+        # return epoch loss
+        # normalizer_train = len(train_loader.dataset)
+        # total_epoch_loss_train = epoch_loss / normalizer_train
+        # return total_epoch_loss_train
+        
+    def evaluate(self, dataset: Dataset, svi: SVI, batch_size: int = 32):
+        # TODO: check device if it is needed or data_val and model already on device
+         
+        loss = 0.
+        for i in range(0, len(dataset), batch_size):
+            j = min([len(dataset), i + batch_size])
+            x = dataset[i:j][0]
+            loss += svi.evaluate_loss(x)
+        
+        loss /= len(dataset)
+        return loss
